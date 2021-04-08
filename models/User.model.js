50a42e3e4d926b15f0962fd2b5cda8df5a7aec80
Luca Manzon <manzonluca@gmail.com>
2021-04-08 19:17:19 +0200

    user-schema

--- models/User.model.js
+++ models/User.model.js
@@ -2,11 +2,34 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  username: {
+  email: {
     type: String,
-    unique: true
+    required: true,
+    unique: true,
   },
-  password: String
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  locations: {
+    type: String,
+    default: "Amsterdam",
+  },
+  shortBio: {
+    type: String,
+  },
+  profilePic: {
+    type: String,
+    default: "",
+  },
 });
 
 const User = model("User", userSchema);
