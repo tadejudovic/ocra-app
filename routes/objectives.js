e8c8b160c594e1a6126cbd6ba4b968a53681f166
Tadej <udovictadej@gmail.com>
2021-04-08 20:29:17 +0100

    changes uploaded

--- routes/objectives.js
+++ routes/objectives.js
@@ -1,5 +1,7 @@
-const router = require("express")
+const router = require("express").Router();
 
 router.get("/objectives", (req, res) => {
   res.render(`/objectives-form`);
 });
+
+module.exports = router;
