c0a23686a80171e22e44ab0500a9af7b5b52fe73
Luca Manzon <manzonluca@gmail.com>
2021-04-10 10:53:16 +0200

    profile

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use("/", index);
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
 
+const profileRoutes = require("./routes/profile");
+app.use("/profile", profileRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
