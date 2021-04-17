7dab0ff3007eee470d001e492548ef3ac4429c0e
Luca Manzon <manzonluca@gmail.com>
2021-04-17 12:00:09 +0200

    edit profile

--- routes/profile.js
+++ routes/profile.js
@@ -9,19 +9,33 @@ router.get("/", isLoggedIn, (req, res, next) => {
   Objective.find({
     user: { $in: req.session.user._id },
   }).then((obj) => {
-    console.log("obj:", obj);
+    //  console.log("obj:", obj);
     res.render("profile", {
       user: req.session.user,
       obj,
-    
     });
   });
 });
 
+router.get("/edit", isLoggedIn, (req, res) => {
+  res.render("edit-profile", { user: req.session.user });
+});
 
+router.post("/edit", isLoggedIn, (req, res) => {
+  const { name, locations, email, surname } = req.body;
+  console.log("edit user:", req.body);
+  User.findByIdAndUpdate(
+    req.session.user._id, // id of the user that was logged in
+    { name, surname, locations, email },
+    { new: true }
+  ).then((newUser) => {
+    // console.log("newUser:", newUser);
+    req.session.user = newUser;
+    res.redirect("/profile");
+  });
+});
 
 module.exports = router;
-
 
 /*
 {
